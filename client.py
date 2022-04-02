a4f9f6c8b417973c8f8eabbf3b75dc55705cdde3
cgy <948628463@qq.com>
2022-04-02 16:01:20 +0800

    fixed some bugs

--- client.py
+++ client.py
@@ -10,8 +10,7 @@ sio = socketio.Client()
 
 @sio.on('connect')
 def connect():
-    # print('connect')
-    pass
+    w.window.deiconify()
 
 
 @sio.on('disconnect')
@@ -31,19 +30,21 @@ def flash_msg(title, message):
 
 @sio.on('msg')
 def handle_msg(data):
+    print(data)
     if data['action'] == 'login':
         if not w.username:
             w.username = data['username']
             w.label_var.set(w.label_var.get() + f" ({w.username})")
-            for name in data['name_list']:
+            for name, sid in data['name_list']:
                 w.add_user(name)
         else:
             flash_msg("有新用户加入了聊天", '欢迎' + data['username'])
             w.add_user(data['username'])
     elif data['action'] == 'logout':
+        print(data['name_list'])
         w.user_list.delete('1.0', 'end')
         w.user_list.insert('end', '在线用户：')
-        for name in data['name_list']:
+        for name, sid in data['name_list']:
             w.add_user(name)
     else:
         if data['username'] != w.username:
@@ -53,13 +54,18 @@ def handle_msg(data):
 
 class ChatWindow:
     def __init__(self):
+        self.sysclose = False
         self.username = None
         self.ip = ''
         # 窗口设置
         self.window = ttkbootstrap.Style(theme='lumen').master
         self.window.title('聊天室')
-        self.window.geometry('1000x600')
-
+        ws = self.window.winfo_screenwidth()
+        hs = self.window.winfo_screenheight()
+        # 计算 x, y 位置
+        x = (ws / 2) - (1000 / 2)
+        y = (hs / 2) - (600 / 2) - 100
+        self.window.geometry(f'1000x600+{int(x)}+{int(y)}')
         # 标题设置
         self.label_var = ttkbootstrap.StringVar()
         self.title = ttkbootstrap.Label(self.window, textvariable=self.label_var,
@@ -81,24 +87,29 @@ class ChatWindow:
         self.msg_box.place(x=30, y=60)
 
         # 用户列表
-        self.user_list = ttkbootstrap.ScrolledText(self.window, width=24, height=28)
+        self.user_list = ttkbootstrap.ScrolledText(self.window, width=24, height=33)
         self.user_list.place(x=780, y=0)
         self.user_list.insert('end', '在线用户：')
 
         # 输入ip
+        self.ipwin = ttkbootstrap.Toplevel(self.window)
+        self.ipwin.title('连接服务器')
+        self.ipwin.geometry('400x80')
         self.inp_var = ttkbootstrap.StringVar()
-        self.ininp = ttkbootstrap.Entry(self.window, textvariable=self.inp_var)
-        self.ipconfirm = ttkbootstrap.Button(self.window, text='确认', command=self.get_ip)
-        self.ininp.place(x=780, y=500)
-        self.ipconfirm.place(x=940, y=500)
+        self.ininp = ttkbootstrap.Entry(self.ipwin, textvariable=self.inp_var)
+        self.ipconfirm = ttkbootstrap.Button(self.ipwin, text='确认', command=self.get_ip)
+        self.ininp.pack()
+        self.ipconfirm.pack()
         self.inp_var.set('请输入要连接服务器的ip')
         self.ininp.bind('<Button-1>', lambda e: self.inp_var.set(''))
+
         # 设置关闭窗口事件
         self.window.protocol("WM_DELETE_WINDOW", self.on_closing)
+        self.ipwin.protocol("WM_DELETE_WINDOW", self.force_close)
 
     def get_ip(self):
-        print(self.ininp.get())
         self.ip = self.ininp.get()
+        self.ipwin.destroy()
 
     def post_msg(self, event=None):
         m = self.get_msg()
@@ -118,11 +129,14 @@ class ChatWindow:
             self.user_list.insert('end', '\n' + ' ' * 5 + username)
 
     def run(self):
+        self.ipwin.place_window_center()
+        self.window.withdraw()
         self.window.mainloop()
 
     def force_close(self):
         try:
             self.window.destroy()
+            self.sysclose = True
             sio.disconnect()
         finally:
             self.window.destroy()
@@ -131,14 +145,8 @@ class ChatWindow:
     def on_closing(self):
         ret = Messagebox.okcancel('关咯', title='提示')
         if ret == '确定':
-            try:
-                sio.emit('msg', {'action': 'logout', 'username': self.username})
-            except Exception as exception:
-                print('关闭时没发出')
-                print(exception)
-            finally:
-                self.window.destroy()
-                sio.disconnect()
+            self.window.destroy()
+            sio.disconnect()
         elif ret == '取消':
             return
 
@@ -156,10 +164,17 @@ if __name__ == '__main__':
     t1.start()
     while w is None or not w.ip:
         time.sleep(0.01)
-    try:
-        sio.connect(f'http://{w.ip}:5000')
-        sio.wait()
-    except Exception as exception:
-        print('没有连接上')
-        print(exception)
-        w.force_close()
+        if w is not None and not w.ip:
+            if w.sysclose:
+                break
+
+    if not w.sysclose:
+        try:
+            sio.connect(f'http://{w.ip}:5000')
+            sio.wait()
+        except Exception as exception:
+            flash_msg(title='连接问题', message='未能连接到服务器')
+            time.sleep(3)
+            print('没有连接上')
+            print(exception)
+            w.force_close()
--- server.py
+++ server.py
@@ -5,14 +5,14 @@ import random
 name_list = []
 
 
-def create_username():
+def create_username(sid):
     with open('name.txt', 'r', encoding='utf-8') as f:
         names = f.readlines()
         names = list(map(lambda x: x.strip(), names))
         name = names[random.randint(0, len(names)-1)]
         while name in name_list:
             name = names[random.randint(0, len(names) - 1)]
-        name_list.append(name)
+        name_list.append((name, sid))
     return name
 
 
@@ -25,26 +25,25 @@ app = socketio.WSGIApp(sio)
 
 @sio.event
 def connect(sid, environ, auth):
-    data = {'username': create_username(), 'action': 'login', 'name_list': name_list}
+    data = {'username': create_username(sid), 'action': 'login', 'name_list': name_list}
     sio.emit('msg', data)
     print('connect ', sid)
 
 
 @sio.event
 def disconnect(sid):
-    # print('disconnect ', sid)
-    pass
+    for i, j in name_list:
+        if j == sid:
+            name_list.remove((i, j))
+    sio.emit('msg', {'action': 'logout', 'name_list': name_list})
+    print(name_list)
+    print('disconnect ', sid)
 
 
 @sio.on('msg')
 def msg(sid, data):
     print(data)
-    if data['action'] == 'logout':
-        name_list.remove(data['username'])
-        data['name_list'] = name_list
-        sio.emit('msg', data)
-    else:
-        sio.emit('msg', data)
+    sio.emit('msg', data)
 
 
 if __name__ == '__main__':
